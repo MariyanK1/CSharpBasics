83cf47188fe4bf291a806104ab267af917879d89
MariyanMaksimov <58628678+MariyanK1@users.noreply.github.com>
2020-06-21 13:18:16 +0200

    Create  numbersFrom1to100.cs

---
+++ Loops/forLoop/numbersFrom1to100.cs
@@ -0,0 +1,13 @@
+/*Write a program that prints the numbers from 1 to 100, each on a new line.*/
+
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+        for (int i = 1; i <= 100; i++) {
+			Console.WriteLine(i);
+		}
+	}
+}
