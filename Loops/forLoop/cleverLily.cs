0af6cd81d25c05c4facbf557eb194dda47e2a193
MariyanMaksimov <58628678+MariyanK1@users.noreply.github.com>
2020-06-21 13:37:42 +0200

    Update cleverLily.cs

--- Loops/forLoop/cleverLily.cs
+++ Loops/forLoop/cleverLily.cs
@@ -1,7 +1,13 @@
 /*
-Lily is already N years old. She receives a gift for each of her birthdays. For odd birthdays (1, 3, 5 ... n) he receives toys, and for each even (2, 4, 6 ... n) he receives money. For the second birthday he receives BGN 10.00, the amount being increased by BGN 10.00, for each subsequent even birthday (2 -> 10, 4 -> 20, 6 -> 30 ... etc.). Over the years, Lily has secretly saved money. Lily's brother, in the years she received money, took BGN 1.00 from them. 
+Lily is already N years old. She receives a gift for each of her birthdays. 
+For odd birthdays (1, 3, 5 ... n) he receives toys, and for each even (2, 4, 6 ... n) he receives money. 
+For the second birthday he receives BGN 10.00, the amount being increased by BGN 10.00, 
+for each subsequent even birthday (2 -> 10, 4 -> 20, 6 -> 30 ... etc.). Over the years, Lily has secretly saved money. 
+Lily's brother, in the years she received money, took BGN 1.00 from them. 
 
-Lily sold the toys received over the years, each for P BGN and added the amount to the money saved. With the money she wanted to buy a washing machine for X leva. Write a program to calculate how much money she has raised and whether she has enough to buy a washing machine.
+Lily sold the toys received over the years, each for P BGN and added the amount to the money saved. 
+With the money she wanted to buy a washing machine for X leva. 
+Write a program to calculate how much money she has raised and whether she has enough to buy a washing machine.
 
 Input
 The program reads 3 numbers entered by the user on separate lines:
